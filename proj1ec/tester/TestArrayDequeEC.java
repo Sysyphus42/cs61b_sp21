d276be8de8694dc4c6e4831b998ea8e8cbe1c9aa
Sysyphus42 <lh1998liuhui@163.com>
2023-12-14 22:08:24 +0800

    done

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -5,31 +5,45 @@ import student.StudentArrayDeque;
 import edu.princeton.cs.algs4.StdRandom;
 public class TestArrayDequeEC {
     @Test
-    public void testBetweenOneAndAHundred() {
+    public void randomizeTest() {
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < 100; i += 1) {
             int num = StdRandom.uniform(1,100);
-            if (num < 50) {
+            int selector = StdRandom.uniform(0,4);
+            if (selector == 0) {
+
                 ads.addFirst(num);
                 sad.addFirst(num);
-            } else {
+                sb.append("addFirst(").append(num).append(")\n");
+
+            } else if (selector == 1) {
                 ads.addLast(num);
                 sad.addLast(num);
+                sb.append("addLast(").append(num).append(")\n");
+
+            } else if (selector == 2) {
+
+                if (!ads.isEmpty()) {
+                    int expected = ads.removeLast();
+                    int actual = sad.removeLast();
+                    sb.append("removeLast()\n");
+                    assertEquals(String.valueOf(sb), expected, actual);
+                }
+
+            }else{
+                if (!ads.isEmpty()) {
+                    int expected = ads.removeFirst();
+                    int actual = sad.removeFirst();
+                    sb.append("removeFirst()\n");
+                    assertEquals(String.valueOf(sb), expected, actual);
+                }
+
             }
         }
 
-        int actual;
-        int expected;
-        for (int i = 0; i < 50; i += 1) {
-            actual = sad.removeFirst();
-            expected = ads.removeFirst();
-            assertEquals(expected, actual);
-        }
-        for (int i = 0; i < 50; i += 1) {
-            actual = sad.removeLast();
-            expected = ads.removeLast();
-            assertEquals(expected, actual);
-        }
+
     }
 }
